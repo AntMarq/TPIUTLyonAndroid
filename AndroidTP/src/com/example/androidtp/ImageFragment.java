e390549fed727e6b5600ac8505b249aceb7bf6c0
Giusti <radament@hotmail.fr>
2013-11-13 15:20:00 +0100

    ajout image fragment

---
+++ AndroidTP/src/com/example/androidtp/ImageFragment.java
@@ -0,0 +1,17 @@
+package com.example.androidtp;
+
+public class ImageFragment {
+
+	public ImageFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
