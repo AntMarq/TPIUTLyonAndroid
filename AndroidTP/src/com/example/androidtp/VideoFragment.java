30d66c2b54659f476e22675febf0554f00f54379
Giusti <radament@hotmail.fr>
2013-11-13 14:42:56 +0100

    TEST GIT

---
+++ AndroidTP/src/com/example/androidtp/VideoFragment.java
@@ -0,0 +1,17 @@
+package com.example.androidtp;
+
+public class VideoFragment {
+
+	public VideoFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
