53415b5171d8b5087c88481cf7e243333928dea2
AntMarq <anthonymarquet21@gmail.com>
2013-11-18 16:14:20 +0100

    VideoFragment

--- AndroidTP/src/com/example/androidtp/VideoFragment.java
+++ AndroidTP/src/com/example/androidtp/VideoFragment.java
@@ -2,19 +2,32 @@ package com.example.androidtp;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-public class VideoFragment extends Fragment 
+public class VideoFragment extends ListFragment 
 {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) 
 	{
 		View view = inflater.inflate (getResources ().getLayout (R.layout.videofragment_layout), container, false);
+		
+		
+		
+		
+		
 		return view;
 	}
+	
+	@Override
+	public void onListItemClick (ListView listView, View view, int position, long id)
+	{
+		
+	}
+	
 
 }
