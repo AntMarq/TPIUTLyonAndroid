d8a8827e79d16ff2b7915852641edca4ce03189e
AntMarq <anthonymarquet21@gmail.com>
2013-11-22 17:49:36 +0100

    Bug commit ^^

---
+++ AndroidTP/src/com/example/androidtp/view/SonCustomAdapter.java
@@ -0,0 +1,85 @@
+package com.example.androidtp.view;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.androidtp.R;
+import com.example.androidtp.R.drawable;
+import com.example.androidtp.R.id;
+import com.example.androidtp.R.layout;
+import com.example.androidtp.model.ObjMediaInfo;
+
+public class SonCustomAdapter extends BaseAdapter
+{
+	private Context 			 mContext;
+	private ArrayList<ObjMediaInfo> newSongList ;
+	private LayoutInflater 		 inflater;
+	
+	public SonCustomAdapter(Context context,ArrayList<ObjMediaInfo> arrayList) 
+	{		
+		mContext = context;
+		inflater = LayoutInflater.from(context);
+		this.newSongList = arrayList ;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return newSongList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return newSongList.get (position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public View getView (int position, View convertView, ViewGroup parent)
+	{
+		ViewHolder holder = null;
+			
+		if (convertView == null)
+		{
+				holder = new ViewHolder();
+				
+				convertView = inflater.inflate(R.layout.son_cell, null);
+				
+				holder.titleVideo = (TextView)convertView.findViewById (R.id.titre);
+				holder.imageVideo = (ImageView)convertView.findViewById (R.id.image);
+				holder.pathVideo = (TextView)convertView.findViewById (R.id.textpath);
+	}						
+	else 
+	{
+		holder = (ViewHolder)convertView.getTag();				
+	}
+		
+		final ObjMediaInfo objMedia = ((ObjMediaInfo)(newSongList.get(position)));
+		holder.titleVideo.setText (objMedia.get_name());
+		holder.imageVideo.setImageResource(R.drawable.music_player);
+		holder.pathVideo.setText( (objMedia.get_url()));
+		
+	return convertView;			
+}
+	
+
+	private class ViewHolder 
+	{
+		TextView 	titleVideo;
+		TextView 	pathVideo;
+		ImageView	imageVideo;
+				
+	}
+}
---
+++ AndroidTP/src/com/example/androidtp/view/TextCustomAdapter.java
@@ -0,0 +1,92 @@
+package com.example.androidtp.view;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.androidtp.R;
+import com.example.androidtp.R.drawable;
+import com.example.androidtp.R.id;
+import com.example.androidtp.R.layout;
+import com.example.androidtp.model.ObjMediaInfo;
+
+public class TextCustomAdapter extends BaseAdapter 
+{
+	
+/**
+ * Adapter for VideoFragment
+ */
+	private Context 			 mContext;
+	private ArrayList<ObjMediaInfo> newtTexList ;
+	private LayoutInflater 		 inflater;
+	
+
+	public TextCustomAdapter(Context context,ArrayList<ObjMediaInfo> arrayList) 
+	{		
+		mContext = context;
+		inflater = LayoutInflater.from(context);
+		this.newtTexList = arrayList ;
+	}
+	
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return newtTexList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return newtTexList.get (position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public View getView (int position, View convertView, ViewGroup parent)
+	{
+		ViewHolder holder = null;
+			
+		if (convertView == null)
+		{
+				holder = new ViewHolder();
+				
+				convertView = inflater.inflate(R.layout.text_cell, null);
+				
+				holder.titleText = (TextView)convertView.findViewById (R.id.titre);
+				holder.imageText = (ImageView)convertView.findViewById (R.id.image);
+				holder.pathText = (TextView)convertView.findViewById (R.id.textpath);
+	}						
+	else 
+	{
+		holder = (ViewHolder)convertView.getTag();				
+	}
+		
+		final ObjMediaInfo objMedia = ((ObjMediaInfo)(newtTexList.get(position)));
+		holder.titleText.setText (objMedia.get_name());
+		holder.imageText.setImageResource(R.drawable.text);
+		holder.pathText.setText( (objMedia.get_url()));
+		
+	return convertView;			
+}
+	
+
+	private class ViewHolder 
+	{
+		TextView 	titleText;
+		TextView 	pathText;
+		ImageView	imageText;
+				
+	}
+
+}
